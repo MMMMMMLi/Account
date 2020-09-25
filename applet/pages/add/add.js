93e61578356152f0e0c95e1babb92876597bfb68
Weijia Jiang <dianxinweizi@163.com>
2020-09-25 17:21:46 +0800

    [51] 新增订单页面绘制完成

--- applet/pages/add/add.js
+++ applet/pages/add/add.js
@@ -16,8 +16,8 @@ Page({
   onShow() {
     // 页面初始化的时候，获取品种和大小的清单
     // REQUEST.request('manage/getCategoryAndSizeArray', 'POST', {})
-      // .then(res => {
-      // })
+    // .then(res => {
+    // })
   },
   // 打开搜索input
   showInput: function () {
@@ -56,10 +56,17 @@ Page({
         that.setData({
           inputUserInfo: res.data.data,
         });
+      } else {
+        wx.showModal({
+          content: res.data.msg,
+          showCancel: false
+        })
       }
     })
   },
   searchClick(e) {
+    // 更新一下组件的订单信息到首页
+    this.updateOrderInfo();
     let oldOrderList = this.data.orderList;
     const clickUserInfo = e.currentTarget.dataset.info;
     clickUserInfo.orderDate = new Date().toLocaleTimeString();
@@ -67,7 +74,10 @@ Page({
       oldOrderList.push({
         orderListId: this.data.orderListId++,
         userInfo: clickUserInfo,
-        orders: [{}]
+        orders: [{
+          categoryValue: 0,
+          sizeValue: 0
+        }]
       })
       this.setData({
         orderList: oldOrderList
@@ -81,8 +91,29 @@ Page({
     }
 
   },
-  testAdd(e) {
-    console.log(e.currentTarget.dataset.name)
+  // 每次搜索获取创建新的订单的时候,都需要拉去一下各个组件最新订单的情况。
+  updateOrderInfo() {
+    let orderList = this.data.orderList;
+    if (orderList.length > 0) {
+      orderList = orderList.map(order => {
+        let thisOrderId = order.orderListId;
+        // 通过组件选择器获取各个组件的实例对象，然后调用其方法。
+        let newOrderInfo = this.selectComponent('#my-addOrder' + thisOrderId).getOrderInfo();
+        if (newOrderInfo.orderListId == thisOrderId) {
+          order = newOrderInfo;
+        }
+        return order;
+      })
+      this.setData({
+        orderList  
+      })
+    }
   },
-
+  // 在每个订单组件提交了之后，都调用这个方法删除当前页面订单信息。
+  deleteOrderInfo(e) {
+    let orderList = this.data.orderList;
+    this.setData({
+      orderList: orderList.filter(order => order.orderListId != e.detail)
+    })
+  },
 })
