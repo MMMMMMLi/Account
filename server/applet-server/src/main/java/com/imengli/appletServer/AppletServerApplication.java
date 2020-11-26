0bec8a8142879a9daba3002cb6b377f3a3e48687
Weijia Jiang <dianxinweizi@163.com>
2020-11-26 10:14:37 +0800

    [101] 减轻测试环境的压力,去除Gateway和Eureka的使用。

--- server/applet-server/src/main/java/com/imengli/appletServer/AppletServerApplication.java
+++ server/applet-server/src/main/java/com/imengli/appletServer/AppletServerApplication.java
@@ -2,10 +2,9 @@ package com.imengli.appletServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 public class AppletServerApplication {
 
     public static void main(String[] args) {
