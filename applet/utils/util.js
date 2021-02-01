3caad30aabccf60f7296c5f12ea24c59949f95d5
Weijia Jiang <dianxinweizi@163.com>
2021-02-01 18:10:29 +0800

    [150] 修改自动计算农历方法

--- applet/utils/util.js
+++ applet/utils/util.js
@@ -85,7 +85,7 @@ function GetcDateString() {
 
 function getLunarDay(solarYear, solarMonth, solarDay) {
   //solarYear = solarYear<1900?(1900+solarYear):solarYear;
-  if (solarYear < 1921 || solarYear > 2020) {
+  if (solarYear < 1921 || solarYear > 2099) {
     return "";
   } else {
     solarMonth = (parseInt(solarMonth) > 0) ? (solarMonth - 1) : 11;
