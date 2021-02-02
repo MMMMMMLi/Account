ac2b12ba4b76f54a6da56680a1667575a0f63b91
Weijia Jiang <dianxinweizi@163.com>
2021-02-02 11:09:10 +0800

    [154] v2021.02版本

--- applet/config.js
+++ applet/config.js
@@ -1,11 +1,11 @@
 module.exports = {
-  version: "v2021.01",
+  version: "v2021.02",
   // 本地测试微服务版
   // preUrl: 'http://localhost:11001/api/'
   // 本地测试单节点版本
-  preUrl: 'http://localhost:10003/devApi/'
+  // preUrl: 'http://localhost:10003/devApi/'
   // 远程调用版本 - 开发版
   // preUrl: 'https://imengli.com/devApi/'
   // 远程调用版本 - 正式版
-  // preUrl: 'https://imengli.com/api/'
+  preUrl: 'https://imengli.com/api/'
 }
